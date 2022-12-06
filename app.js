ed7f90d080ad737ce3ea0c157d8be37af12c6cf7
tiagurb <tiagurb@hotmail.com>
2022-12-06 21:33:43 +0000

    dentinho routes

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 // ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
 require("dotenv").config();
 
 // ℹ️ Connects to the database
 require("./db");
 
 // Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
 const express = require("express");
 
 // Handles the handlebars
-// https://www.npmjs.com/package/hbs
 const hbs = require("hbs");
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
+//require('./config/session.config')(app);
 
 // default value for title local
 const capitalize = require("./utils/capitalize");
@@ -30,11 +28,12 @@ const authRoutes = require('./routes/auth.routes');
 const barRoutes = require('./routes/bar.routes');
 const dentinhoRoutes = require('./routes/dentinho.routes');
 
+
 app.use("/", indexRoutes);
 app.use('/auth', authRoutes);
 
 app.use('/bar', barRoutes);
-app.use('/dentinho', dentinhoRoutes);
+app.use('/', dentinhoRoutes);
 
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
